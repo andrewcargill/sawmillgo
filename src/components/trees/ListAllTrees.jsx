707a7aae540d8c405f182eab921112b61c47489b
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-04-10 06:58:04 +0200

    add tree ID filter to all trees component

--- src/components/trees/ListAllTrees.jsx
+++ src/components/trees/ListAllTrees.jsx
@@ -10,21 +10,27 @@ import { app } from "../../firebase-config"; // Update the import path as necess
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import FormHelperText from "@mui/material/FormHelperText";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
+import TextField from "@mui/material/TextField";
 import TreeDetailsModal from "./TreeDetailsModal";
 import TreeListModal from "./TreeListModal";
 import { fetchSpeciesForSawmill } from "../../utils/filestoreOperations";
+import { Add } from "@mui/icons-material";
+
 
 const ListAllTrees = () => {
   const [trees, setTrees] = useState([]);
   const [filter, setFilter] = useState("all");
   const [loggingFilter, setLoggingFilter] = useState("all");
   const [speciesFilter, setSpeciesFilter] = useState("all");
+  const [refIdFilter, setRefIdFilter] = useState("");
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTreeDetails, setSelectedTreeDetails] = useState(null);
   const [species, setSpecies] = useState([]);
@@ -61,34 +67,37 @@ const ListAllTrees = () => {
       console.log("Sawmill ID not found. Cannot fetch trees.");
       return;
     }
-  
+
     let q = collection(db, `sawmill/${sawmillId}/trees`);
-  
+
     let conditions = []; // Array to hold query conditions
     // Add condition for logging status
-    if (loggingFilter === "logged") {
-          q = query(q, where("logged", "==", true));
-        } else if (loggingFilter === "unlogged") {
-          q = query(q, where("logged", "==", false));
-        }
-  
-    if (speciesFilter !== "all") {
-      conditions.push(where("speciesId", "==", speciesFilter));
+
+    if (refIdFilter.trim()) {
+      // Filter by refId if it is provided
+      q = query(q, where("refId", "==", refIdFilter.trim()));
+    } else {
+      if (loggingFilter === "logged") {
+        q = query(q, where("logged", "==", true));
+      } else if (loggingFilter === "unlogged") {
+        q = query(q, where("logged", "==", false));
+      }
+
+      if (speciesFilter !== "all") {
+        conditions.push(where("speciesId", "==", speciesFilter));
+      }
+
+      if (conditions.length > 0) {
+        q = query(q, ...conditions);
+      }
     }
-  
-    if (conditions.length > 0) {
-      q = query(q, ...conditions);
-    }
-  
     const snapshot = await getDocs(q);
-    const treesList = snapshot.docs.map(doc => ({
+    const treesList = snapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
     setTrees(treesList);
   };
-  
-
 
   useEffect(() => {
     fetchSpeciesForSawmill(db, sawmillId)
@@ -103,7 +112,7 @@ const ListAllTrees = () => {
 
   useEffect(() => {
     fetchTrees();
-  }, [loggingFilter, speciesFilter]); // Re-run fetchTrees when filter changes
+  }, [loggingFilter, speciesFilter, refIdFilter]); // Re-run fetchTrees when filter changes
 
   const handleTreeClick = (treeId) => {
     const tree = trees.find((t) => t.id === treeId);
@@ -117,17 +126,42 @@ const ListAllTrees = () => {
 
   return (
     <Grid container spacing={2} border={1} p={2}>
-      <Grid item xs={12}>
-        <Typography variant="h4" color="initial">
-          Trees
-        </Typography>
+      <Grid container item xs={12}>
+        <Grid xs={10} container item justifyContent={'start'}>
+          <Typography variant="h4" color="initial">
+            Trees
+          </Typography>
+        </Grid>
+        <Grid xs={2}>
+         <Button
+           variant="outlined"
+           color="primary"
+           startIcon={<AddIcon />}
+           
+         >
+           add
+         </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={4}>
+        <FormControl fullWidth>
+          <TextField
+            size="small"
+            label="Tree ID"
+            variant="outlined"
+            value={refIdFilter}
+            onChange={(e) => setRefIdFilter(e.target.value.toUpperCase())}
+            helperText="Enter a Ref ID to filter by specific tree"
+          />
+        </FormControl>
+      </Grid>
+      <Grid item xs={4}>
         <FormControl fullWidth>
           <InputLabel id="logging-filter-select-label">
             Logging Status
           </InputLabel>
           <Select
+            size="small"
             labelId="logging-filter-select-label"
             id="logging-filter-select"
             value={loggingFilter}
@@ -140,10 +174,11 @@ const ListAllTrees = () => {
           </Select>
         </FormControl>
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={4}>
         <FormControl fullWidth>
           <InputLabel id="species-filter-select-label">Species</InputLabel>
           <Select
+            size="small"
             labelId="species-filter-select-label"
             id="species-filter-select"
             value={speciesFilter}
