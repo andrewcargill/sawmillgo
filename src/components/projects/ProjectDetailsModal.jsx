75531a9ad2c874f3858e481a13e9dca0eb354752
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-04-11 19:44:47 +0200

    begin styling of project details

--- src/components/projects/ProjectDetailsModal.jsx
+++ src/components/projects/ProjectDetailsModal.jsx
@@ -10,27 +10,103 @@ import {
 import CancelIcon from "@mui/icons-material/Cancel";
 import { deleteDoc, doc, getFirestore } from "firebase/firestore";
 import { app } from "../../firebase-config";
+import {
+  Paper,
+  TableContainer,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
+import Chip from "@mui/material/Chip";
 
-
-
-function ProjectDetailsModal({ treeDetails, onClose, setMode }) {
-
+function ProjectDetailsModal({ projectDetails, onClose, setMode }) {
   const userLocalStorage = JSON.parse(localStorage.getItem("user"));
   const db = getFirestore(app);
   const sawmillId = userLocalStorage?.sawmillId;
 
-
   const handleEditButtonClick = () => {
     setMode("edit");
-  }
-  
-  
+  };
+
   return (
-    <Grid>
-    Project Details Modal
-    <Button variant="contained" onClick={handleEditButtonClick}>
-                Edit
-              </Button>
+    <Grid container>
+      <Grid cvontainer item xs={12}>
+        <Grid item xs={8}>
+          <Typography
+            id="tree-details-title"
+            variant="h6"
+            component="h2"
+            style={{ textTransform: "capitalize" }}
+          >
+            {projectDetails.projectName}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Chip label={projectDetails.status} />
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                  Created By
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                  {projectDetails.createdBy}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                  Customer Name
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                  {projectDetails.customerName}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                  Project Info:
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                  {projectDetails.projectInfo}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                  Project Notes:
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                  {projectDetails.notes}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                  Date Started
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                  {projectDetails.date}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                  Deadline
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                  {projectDetails.deadline}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      <Grid item xs={6}></Grid>
+
+      <Button variant="contained" onClick={handleEditButtonClick}>
+        Edit
+      </Button>
     </Grid>
   );
 }
--- src/components/projects/ProjectListModal.jsx
+++ src/components/projects/ProjectListModal.jsx
@@ -1,48 +1,53 @@
 import React, { useState, useEffect } from "react";
-import {
-  Modal,
-  Box,
-} from "@mui/material";
+import { Modal, Box } from "@mui/material";
 import ProjectDetailsModal from "./ProjectDetailsModal";
 import ProjectAddEditModal from "./ProjectAddEditModal";
 // import ProjectDetailsModal from "./ProjectDetailsModal"; // Assuming this is for viewing project details
 // import AddEditProjectForm from "../components-for-dev/projects/AddEditProjectForm"; // Adjust the import path as needed
 
 function ProjectListModal({ isOpen, onClose, projectDetails, mode, setMode }) {
-    
-    useEffect(() => {
-        if (!isOpen) {
-            setMode('view'); // Reset mode to view when modal is closed
-        }
-    }, [isOpen, setMode]);
+  useEffect(() => {
+    if (!isOpen) {
+      setMode("view"); // Reset mode to view when modal is closed
+    }
+  }, [isOpen, setMode]);
 
-    return (
-        <Modal
-            open={isOpen}
+  return (
+    <Modal
+      open={isOpen}
+      onClose={onClose}
+      aria-labelledby="project-details-title"
+      aria-describedby="project-details-description"
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: "background.paper",
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        {mode === "view" && (
+          <ProjectDetailsModal
+            setMode={setMode}
+            projectDetails={projectDetails}
             onClose={onClose}
-            aria-labelledby="project-details-title"
-            aria-describedby="project-details-description"
-        >
-            <Box
-                sx={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    width: 400,
-                    bgcolor: "background.paper",
-                    boxShadow: 24,
-                    p: 4,
-                }}
-            >
-              Project List Modal 
-        
-                {mode === 'view' && <ProjectDetailsModal setMode={setMode} projectDetails={projectDetails} onClose={onClose} />}
-                {mode === 'edit' && <ProjectAddEditModal projectDetails={projectDetails} onClose={onClose} />}
-                {mode === 'add' && <ProjectAddEditModal onClose={onClose} />}
-            </Box>
-        </Modal>
-    );
+          />
+        )}
+        {mode === "edit" && (
+          <ProjectAddEditModal
+            projectDetails={projectDetails}
+            onClose={onClose}
+          />
+        )}
+        {mode === "add" && <ProjectAddEditModal onClose={onClose} />}
+      </Box>
+    </Modal>
+  );
 }
 
 export default ProjectListModal;
