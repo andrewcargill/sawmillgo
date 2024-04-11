aeb2d6db8874cf353af6b26b91fe7f49d2119e45
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-04-11 22:30:34 +0200

    add CRUD to projects page

--- src/components/projects/ProjectAddEditModal.jsx
+++ src/components/projects/ProjectAddEditModal.jsx
@@ -4,29 +4,228 @@ import {
   Box,
   Typography,
   Button,
+  TextField,
   Grid,
   IconButton,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
-import CancelIcon from "@mui/icons-material/Cancel";
-import { deleteDoc, doc, getFirestore } from "firebase/firestore";
+import { doc, setDoc, updateDoc, addDoc, collection, getFirestore } from "firebase/firestore";
 import { app } from "../../firebase-config";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { getAuth } from 'firebase/auth';
 
 
+function ProjectAddEditModal({ projectDetails, isOpen, onClose }) {
 
-function ProjectAddEditModal({ treeDetails, onClose, setMode }) {
 
-  const userLocalStorage = JSON.parse(localStorage.getItem("user"));
+  const [isLoading, setIsLoading] = useState(false);
+  const [formValues, setFormValues] = useState({
+    sawmillId: '',
+    projectName: '',
+    createdBy: '',
+    creatorId: '',
+    customerName: '',
+    projectInfo: '',
+    notes: '',
+    date: '',
+    deadline: '',
+    status: 'active',
+  });
+
   const db = getFirestore(app);
+  const auth = getAuth(app);
+  const currentUserUID = auth.currentUser ? auth.currentUser.uid : null;
+  const userLocalStorage = JSON.parse(localStorage.getItem("user"));
   const sawmillId = userLocalStorage?.sawmillId;
- 
+  
 
+
+
+  useEffect(() => {
+    if (projectDetails) {
+      setFormValues({
+        projectName: projectDetails.projectName || '',
+        createdBy: projectDetails.createdBy || '',
+        creatorId: projectDetails.creatorId || '',
+        customerName: projectDetails.customerName || '',
+        projectInfo: projectDetails.projectInfo || '',
+        notes: projectDetails.notes || '',
+        date: projectDetails.date || '',
+        deadline: projectDetails.deadline || '',
+        status: projectDetails.status || 'active',
+      });
+    }
+  }, [projectDetails]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true); // Assume you have a state [isLoading, setIsLoading] = useState(false)
+  
+    const userLocalStorage = JSON.parse(localStorage.getItem("user"));
+    const sawmillId = userLocalStorage?.sawmillId;
+  
+    if (!sawmillId) {
+      console.error("Sawmill ID is not available. Cannot modify the project.");
+      alert("Sawmill ID is not available. Cannot modify the project.");
+      setIsLoading(false);
+      return;
+    }
+  
+    // Extend with additional validations as needed
+    if (!formValues.projectName || !formValues.customerName) {
+      console.error("Required fields are missing.");
+      alert("Please fill all required fields.");
+      setIsLoading(false);
+      return;
+    }
+  
+    const projectData = {
+      ...formValues,
+      createdBy: projectDetails ? formValues.createdBy : currentUserUID, // Maintain createdBy for updates, set for new entries
+    };
+  
+    try {
+      if (projectDetails) {
+        // Updating an existing project
+        const projectRef = doc(db, `sawmill/${sawmillId}/projects`, projectDetails.id);
+        await updateDoc(projectRef, projectData);
+        alert('Project updated successfully!');
+      } else {
+        // Adding a new project
+        const newProjectData = {
+          ...projectData,
+          sawmillId: sawmillId, // Including sawmillId for new projects
+        };
+        await addDoc(collection(db, `sawmill/${sawmillId}/projects`), newProjectData);
+        alert('Project added successfully!');
+      }
+      onClose(); // Close modal after operation
+    } catch (error) {
+      console.error("Error saving project: ", error);
+      alert(`Failed to save project. Error: ${error.message}`);
+    }
+    setIsLoading(false); // Reset loading state
+  };
+  
+  
+  
 
   return (
-    <Grid>
-    Project Add / Edit Modal
  
+      <Box>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          {projectDetails ? 'Edit Project' : 'Add New Project'}
+        </Typography>
+        <Grid container spacing={2} mt={2}>
 
-    </Grid>
+        <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel id="status-label">Status</InputLabel>
+              <Select
+                labelId="status-label"
+                id="status-select"
+                name="status"
+                value={formValues.status}
+                label="Status"
+                onChange={handleChange}
+              >
+                <MenuItem value="active">Active</MenuItem>
+                <MenuItem value="paused">Paused</MenuItem>
+                <MenuItem value="with creator">With Creator</MenuItem>
+                <MenuItem value="sold">Sold</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Project Name"
+              name="projectName"
+              value={formValues.projectName}
+              onChange={handleChange}
+            />
+          </Grid>
+         
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Customer Name"
+              name="customerName"
+              value={formValues.customerName}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Creator ID"
+              name="creatorId"
+              value={formValues.creatorId}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Project Info"
+              name="projectInfo"
+              value={formValues.projectInfo}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Notes"
+              name="notes"
+              value={formValues.notes}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              type="date"
+              label="Date Started"
+              name="date"
+              value={formValues.date}
+              onChange={handleChange}
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              type="date"
+              label="Deadline"
+              name="deadline"
+              value={formValues.deadline}
+              onChange={handleChange}
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item xs={12} mt={2}>
+            <Button variant="contained" onClick={handleSubmit}>
+              {projectDetails ? 'Update Project' : 'Add Project'}
+            </Button>
+            <IconButton aria-label="close" onClick={onClose}>
+              <CancelIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Box>
+  
   );
 }
 
--- src/components/projects/ProjectDetailsModal.jsx
+++ src/components/projects/ProjectDetailsModal.jsx
@@ -19,11 +19,33 @@ import {
   TableCell,
 } from "@mui/material";
 import Chip from "@mui/material/Chip";
+import TagFacesIcon from '@mui/icons-material/TagFaces';
 
 function ProjectDetailsModal({ projectDetails, onClose, setMode }) {
   const userLocalStorage = JSON.parse(localStorage.getItem("user"));
   const db = getFirestore(app);
   const sawmillId = userLocalStorage?.sawmillId;
+  const projectUid = projectDetails?.id;
+
+  const handleDelete = async () => {
+    // Use prompt to ask the user to input the refId
+    const userInput = window.prompt(
+      `This action will delete ${projectDetails.projectName}. All linked stock will have status set to 'avaliable'.`
+    );
+
+    if (userInput === projectDetails.projectName) {
+      try {
+        if (projectUid) {
+          await deleteDoc(doc(db, `sawmill/${sawmillId}/projects`, projectUid));
+          alert("Project deleted successfully.");
+          onClose(); // Close the modal or redirect user
+        }
+      } catch (error) {
+        console.error("Error deleting project: ", error);
+        alert(`Failed to delete project. Error: ${error.message}`);
+      }
+    }
+  };
 
   const handleEditButtonClick = () => {
     setMode("edit");
@@ -31,22 +53,23 @@ function ProjectDetailsModal({ projectDetails, onClose, setMode }) {
 
   return (
     <Grid container>
-      <Grid cvontainer item xs={12}>
+      <Grid container item xs={12} mb={2}>
         <Grid item xs={8}>
           <Typography
             id="tree-details-title"
             variant="h6"
             component="h2"
+            color={"primary"}
             style={{ textTransform: "capitalize" }}
           >
             {projectDetails.projectName}
           </Typography>
         </Grid>
-        <Grid item xs={4}>
-          <Chip label={projectDetails.status} />
+        <Grid container item xs={4} justifyContent={'end'}>
+          <Chip size="small" color="secondary" style={{ textTransform: 'capitalize'}} label={projectDetails.status} />
         </Grid>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} pb={2}>
         <TableContainer component={Paper}>
           <Table>
             <TableBody>
@@ -60,7 +83,15 @@ function ProjectDetailsModal({ projectDetails, onClose, setMode }) {
               </TableRow>
               <TableRow>
                 <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
-                  Customer Name
+                  Creator
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                  {projectDetails.creatorId}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                  Customer
                 </TableCell>
                 <TableCell sx={{ py: 0.5, px: 1 }}>
                   {projectDetails.customerName}
@@ -68,7 +99,7 @@ function ProjectDetailsModal({ projectDetails, onClose, setMode }) {
               </TableRow>
               <TableRow>
                 <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
-                  Project Info:
+                   Info
                 </TableCell>
                 <TableCell sx={{ py: 0.5, px: 1 }}>
                   {projectDetails.projectInfo}
@@ -76,7 +107,7 @@ function ProjectDetailsModal({ projectDetails, onClose, setMode }) {
               </TableRow>
               <TableRow>
                 <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
-                  Project Notes:
+                  Notes
                 </TableCell>
                 <TableCell sx={{ py: 0.5, px: 1 }}>
                   {projectDetails.notes}
@@ -89,6 +120,8 @@ function ProjectDetailsModal({ projectDetails, onClose, setMode }) {
                 <TableCell sx={{ py: 0.5, px: 1 }}>
                   {projectDetails.date}
                 </TableCell>
+                
+                
               </TableRow>
               <TableRow>
                 <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
@@ -102,11 +135,69 @@ function ProjectDetailsModal({ projectDetails, onClose, setMode }) {
           </Table>
         </TableContainer>
       </Grid>
-      <Grid item xs={6}></Grid>
+      <Grid item xs={12}>
+        <TableContainer component={Paper}>
+            <Grid container item xs={12} p={1}>
+        <Typography
+            id="tree-details-title"
+            variant="body2"
+            component="h2"
+            style={{ textTransform: "capitalize" }}
+          >
+            allocated stock
+          </Typography>
+          </Grid>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                 TREES
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                 LOGS
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                 PLANKS
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ width: "30%", py: 0.5, px: 1 }}>
+                 <div>XXFN</div> 
+                 <div>XXFN</div> 
+                 <div>XXFN</div> 
+                 <div>XXFN</div> 
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                XXFL
+                </TableCell>
+                <TableCell sx={{ py: 0.5, px: 1 }}>
+                 GGFD
+                </TableCell>
+              </TableRow>
+              </TableBody>
+              </Table>
+              </TableContainer>
+              </Grid>
 
-      <Button variant="contained" onClick={handleEditButtonClick}>
-        Edit
-      </Button>
+      <Grid
+              container
+              pt={4}
+              flexDirection={"row"}
+              justifyContent={"space-around"}
+            >
+              <Button variant="contained" onClick={handleEditButtonClick}>
+                Edit
+              </Button>
+
+              <Button variant="contained" color="warning" onClick={handleDelete}>
+                Delete
+              </Button>
+
+              <IconButton aria-label="" onClick={onClose}>
+                <CancelIcon />
+              </IconButton>
+            </Grid>
+
     </Grid>
   );
 }
