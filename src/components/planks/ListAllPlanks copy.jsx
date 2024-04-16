4e9e6fae751670545cec5cea2048cc32f18135da
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-04-16 20:13:28 +0200

    create different view options for planks

--- src/components/planks/ListAllPlanks copy.jsx
+++ src/components/planks/ListAllPlanks copy.jsx
@@ -6,13 +6,37 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
+import PlankListContent from "./sub-components/PlankListContent";
+import GridOnIcon from '@mui/icons-material/GridOn';
+import GridOffIcon from '@mui/icons-material/GridOff';
+import ListIcon from '@mui/icons-material/List';
+import AppsIcon from '@mui/icons-material/Apps';
 
 const ListAllPlanks = () => {
   const [planks, setPlanks] = useState([]);
+  const [dynamicView, setDynamicView] = useState('dynamic');
 
   const db = getFirestore(app);
   const sawmillId = JSON.parse(localStorage.getItem("user"))?.sawmillId;
   const navigate = useNavigate();
+
+  const views = [
+    { view: 'dynamic', icon: <AppsIcon />},
+    { view: 'basic', icon:  <GridOnIcon /> },
+    { view: 'list', icon: <ListIcon /> }
+  ];
+
+  const handleDynamicViewClick = () => {
+    // Find the index of the current view
+    const currentIndex = views.findIndex(v => v.view === dynamicView);
+    // Calculate the next index, cycling back to 0 if at the end
+    const nextIndex = (currentIndex + 1) % views.length;
+    // Update the state to the next view
+    setDynamicView(views[nextIndex].view);
+  };
+
+  // Get the current icon based on the dynamicView
+  const currentIcon = views.find(v => v.view === dynamicView)?.icon;
 
   const fetchPlanks = async () => {
     if (!sawmillId) {
@@ -46,8 +70,86 @@ const ListAllPlanks = () => {
   const handlePlankClick = (plankId) => {
     return () => {
       navigate(`/plank/${plankId}`);
+      console.log('Clicked')
     };
   };
+
+  // const handleDynamicViewClick = () => {
+  //   setDynamicView((prev) => !prev);
+  // }
+
+  function renderPlankView(plank, view) {
+    switch (view) {
+      case 'dynamic':
+        return (
+          <Grid
+            key={plank.id}
+            onClick={handlePlankClick(plank.id)}
+          >
+            <PlankListContent data={plank} />
+          </Grid>
+        );
+      case 'basic':
+        return (
+          <Grid
+            className="item-select"
+            item
+            container
+            xs={3}
+            sm={2}
+            lg={2}
+            key={plank.id}
+            m={1}
+            bgcolor="white.main"
+            sx={{
+              border: plank.verified ? "4px solid green" : "2px solid lightgrey",
+              borderRadius: "5px",
+              padding: "12px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              cursor: "pointer",
+            }}
+            onClick={handlePlankClick(plank.id)}
+          >
+            <Grid item>
+              <h3>{plank.refId}</h3>
+            </Grid>
+            <Grid item>
+              <p>{plank.speciesName}</p>
+            </Grid>
+          </Grid>
+        );
+      case 'list':
+        return (
+          <Grid
+            key={plank.id}
+            container
+            spacing={2}
+            sx={{
+              padding: "8px",
+              border: "1px solid lightgrey",
+              borderRadius: "4px",
+              backgroundColor: "white",
+              margin: "4px",
+              cursor: "pointer",
+            }}
+            onClick={handlePlankClick(plank.id)}
+          >
+            <Grid item xs={12}>
+              <Typography variant="h6">{plank.refId}</Typography>
+              <Typography variant="body2">{plank.speciesName}</Typography>
+            </Grid>
+          </Grid>
+        );
+      default:
+        return null;
+    }
+  }
+
+
+
 
   return (
     <Grid container spacing={2} p={2}>
@@ -58,6 +160,13 @@ const ListAllPlanks = () => {
           </Typography>
         </Grid>
         <Grid container item xs={6} sm={2} justifyContent={"end"}>
+        <Button
+      variant="outlined"
+      color="primary"
+      onClick={handleDynamicViewClick}
+    >
+      {currentIcon}
+    </Button>
           <Button
             variant="outlined"
             color="primary"
@@ -71,52 +180,26 @@ const ListAllPlanks = () => {
       <Grid item xs={12}>
         Last added plank: {planks.length > 0 ? planks[0].refId : "No planks available"}
       </Grid>
-
-      <Grid
-        container
-        sx={{ justifyContent: { xs: "center", sm: "flex-start" } }}
-        alignContent={"center"}
-      >
-        {planks.length > 0 ? (
-          planks.map((plank) => (
-            <Grid
-              className="item-select"
-              item
-              container
-              xs={3}
-              sm={2}
-              lg={2}
-              key={plank.id}
-              m={1}
-              bgcolor={"white.main"}
-              style={{
-                border: plank.verified ? "4px solid green" : "2px solid lightgrey",
-                borderRadius: "5px",
-                padding: "12px",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                cursor: "pointer",
-              }}
-              onClick={handlePlankClick(plank.id)}
-            >
-              <Grid item>
-                <h3>{plank.refId}</h3>
-              </Grid>
-              <Grid item>
-                <p>{plank.date}</p>
-              </Grid>
+          <Grid>
+              <Grid
+          container
+          sx={{ justifyContent: { xs: "center", sm: "flex-start" } }}
+          alignContent="center"
+          
+        >
+          {planks.length > 0 ? (
+            planks.map((plank) => (
+              renderPlankView(plank, dynamicView)
+            ))
+          ) : (
+            <Grid item xs={12}>
+              <Typography variant="body1">No planks found.</Typography>
             </Grid>
-          ))
-       
-        ) : (
-          <Grid item xs={12}>
-            <Typography variant="body1">No planks found.</Typography>
-          </Grid>
-        )}
-      </Grid>
+          )}
+        </Grid>
+        </Grid>
     </Grid>
+
   );
 };
 
--- src/components/planks/ListAllPlanks.jsx
+++ src/components/planks/ListAllPlanks.jsx
@@ -1,42 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { getFirestore, collection, getDocs, query, orderBy } from "firebase/firestore";
-import { app } from "../../firebase-config"; // Correct the import path as necessary
+import { app } from "../../firebase-config";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
 import PlankListContent from "./sub-components/PlankListContent";
+import AppsIcon from '@mui/icons-material/Apps';
 import GridOnIcon from '@mui/icons-material/GridOn';
 import GridOffIcon from '@mui/icons-material/GridOff';
 import ListIcon from '@mui/icons-material/List';
-import AppsIcon from '@mui/icons-material/Apps';
+import { Table, TableHead, TableRow, TableCell, TableContainer, Paper, TableBody } from "@mui/material";
+
 
 const ListAllPlanks = () => {
   const [planks, setPlanks] = useState([]);
   const [dynamicView, setDynamicView] = useState('dynamic');
-
   const db = getFirestore(app);
   const sawmillId = JSON.parse(localStorage.getItem("user"))?.sawmillId;
   const navigate = useNavigate();
 
   const views = [
-    { view: 'dynamic', icon: <AppsIcon />},
-    { view: 'basic', icon:  <GridOnIcon /> },
+    { view: 'dynamic', icon: <AppsIcon /> },
+    { view: 'basic', icon: <GridOnIcon /> },
     { view: 'list', icon: <ListIcon /> }
   ];
 
   const handleDynamicViewClick = () => {
-    // Find the index of the current view
     const currentIndex = views.findIndex(v => v.view === dynamicView);
-    // Calculate the next index, cycling back to 0 if at the end
     const nextIndex = (currentIndex + 1) % views.length;
-    // Update the state to the next view
     setDynamicView(views[nextIndex].view);
   };
-
-  // Get the current icon based on the dynamicView
-  const currentIcon = views.find(v => v.view === dynamicView)?.icon;
 
   const fetchPlanks = async () => {
     if (!sawmillId) {
@@ -44,56 +39,52 @@ const ListAllPlanks = () => {
       return;
     }
 
-    let q = query(
+    const q = query(
       collection(db, `sawmill/${sawmillId}/planks`),
       orderBy("createdAt", "desc")
     );
     const snapshot = await getDocs(q);
-    const planksList = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    const planksList = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     setPlanks(planksList);
-    console.log("Fetched planks: ", planksList);
   };
 
   useEffect(() => {
     fetchPlanks();
-  }, [sawmillId]); // Fetch planks when component mounts and sawmillId changes
+  }, [sawmillId]);
 
   const handleAddPlankClick = () => {
     navigate("/addplank");
   };
 
-
-
-  const handlePlankClick = (plankId) => {
-    return () => {
-      navigate(`/plank/${plankId}`);
-    };
+  const handlePlankClick = plankId => () => {
+    navigate(`/plank/${plankId}`);
   };
 
-  // const handleDynamicViewClick = () => {
-  //   setDynamicView((prev) => !prev);
-  // }
-
-  function renderPlankView(plank, view) {
+  function renderPlankView(view) {
     switch (view) {
       case 'dynamic':
-        return (
-          <Grid
-            key={plank.id}
-            onClick={() => handlePlankClick(plank.id)}
-          >
+        return( 
+          
+          <Grid container >
+          {planks.map(plank => (
+          
+            <Grid 
+            onClick={handlePlankClick(plank.id)}
+            >
+         
             <PlankListContent data={plank} />
-          </Grid>
-        );
+            </Grid>
+       
+        ))}
+        </Grid>
+      );
       case 'basic':
         return (
+        <Grid container >
+        {planks.map(plank => (
           <Grid
             className="item-select"
             item
-            container
             xs={3}
             sm={2}
             lg={2}
@@ -110,93 +101,94 @@ const ListAllPlanks = () => {
               alignItems: "center",
               cursor: "pointer",
             }}
-            onClick={() => handlePlankClick(plank.id)}
+            onClick={handlePlankClick(plank.id)}
           >
             <Grid item>
-              <h3>{plank.refId}</h3>
-            </Grid>
-            <Grid item>
-              <p>{plank.speciesName}</p>
-            </Grid>
-          </Grid>
-        );
-      case 'list':
-        return (
-          <Grid
-            key={plank.id}
-            container
-            spacing={2}
-            sx={{
-              padding: "8px",
-              border: "1px solid lightgrey",
-              borderRadius: "4px",
-              backgroundColor: "white",
-              margin: "4px",
-              cursor: "pointer",
-            }}
-            onClick={() => handlePlankClick(plank.id)}
-          >
-            <Grid item xs={12}>
               <Typography variant="h6">{plank.refId}</Typography>
               <Typography variant="body2">{plank.speciesName}</Typography>
             </Grid>
           </Grid>
+        ))}
+      </Grid>
+      );
+      case 'list':
+        return (
+          <TableContainer component={Paper}>
+          <Table aria-label="simple table" sx={{ minWidth: 800 }} >
+            <TableHead>
+              <TableRow>
+                <TableCell>Ref ID</TableCell>
+                <TableCell>Quality</TableCell>
+                <TableCell>Species</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Grade</TableCell>
+                <TableCell>Width</TableCell>
+                <TableCell>Depth</TableCell>
+                <TableCell>Length</TableCell>
+                <TableCell>Location</TableCell>
+                <TableCell>Project</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {planks.map((plank) => (
+                <TableRow
+                  key={plank.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  hover
+                  onClick={handlePlankClick(plank.id)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <TableCell component="th" scope="row">
+                    {plank.refId}
+                  </TableCell>
+                  <TableCell>{plank.verified ? 'verified' : 'standard'}</TableCell>
+                  <TableCell>{plank.speciesName}</TableCell>
+                  <TableCell>{plank.status}</TableCell>
+                  <TableCell>{plank.grade}</TableCell>
+                  <TableCell>{plank.width}</TableCell>
+                  <TableCell>{plank.depth}</TableCell>
+                  <TableCell>{plank.length}</TableCell>
+                  <TableCell>{plank.locationName}</TableCell>
+                  <TableCell>{plank.projectName}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
         );
       default:
         return null;
     }
   }
 
-
-
-
   return (
     <Grid container spacing={2} p={2}>
-      <Grid container item xs={12}>
-        <Grid xs={6} sm={10} container item justifyContent={"start"}>
-          <Typography variant="h4" color="initial">
-            Planks
-          </Typography>
-        </Grid>
-        <Grid container item xs={6} sm={2} justifyContent={"end"}>
+      <Grid container item xs={12} justifyContent="space-between" alignItems="center">
+        <Typography variant="h4">Planks</Typography>
         <Button
-      variant="outlined"
-      color="primary"
-      onClick={handleDynamicViewClick}
-    >
-      {currentIcon}
-    </Button>
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={handleAddPlankClick}
-            startIcon={<AddIcon />}
-          >
-            Add
-          </Button>
-        </Grid>
+          variant="outlined"
+          color="primary"
+          onClick={handleDynamicViewClick}
+          startIcon={views.find(v => v.view === dynamicView)?.icon}
+        >
+          Change View
+        </Button>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={handleAddPlankClick}
+          startIcon={<AddIcon />}
+        >
+          Add Plank
+        </Button>
       </Grid>
       <Grid item xs={12}>
         Last added plank: {planks.length > 0 ? planks[0].refId : "No planks available"}
       </Grid>
-
-              <Grid
-          container
-          sx={{ justifyContent: { xs: "center", sm: "flex-start" } }}
-          alignContent="center"
-        >
-          {planks.length > 0 ? (
-            planks.map((plank) => (
-              renderPlankView(plank, dynamicView)
-            ))
-          ) : (
-            <Grid item xs={12}>
-              <Typography variant="body1">No planks found.</Typography>
-            </Grid>
-          )}
-        </Grid>
+      <Grid item xs={12}>
+        {planks.length > 0 ? renderPlankView(dynamicView) : <Typography variant="body1">No planks found.</Typography>}
+      </Grid>
     </Grid>
-
   );
 };
 
