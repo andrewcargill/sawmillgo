953a2fd61586a726a1442941a4ecc2d293a2c1d5
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-04-20 20:15:26 +0200

    developing homepage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
+import { Paper } from '@mui/material';
 
 const AboutPage = () => {
  
 
   return (
-    <Grid container bgcolor={'lightblue'}>
-     <Typography variant="h1" color="initial">About</Typography>
+    <Grid container>
+      <Paper sx={{padding: '15px'}} >
+     <Typography variant="h4" align='left' color="initial">Transform Your Forestry Business with Sawmill Go</Typography>
+     <Typography variant="h5" align='left'  color="initial">A cloud-based forestry management system that delivers complete transparency and the story behind your wood products. Enhance the value of every 
+     tree with detailed tracking from forest to final product, ensuring sustainability and maximizing your profits.</Typography>
+     </Paper>
     </Grid>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,76 @@
 import React from 'react';
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
+import { List, ListItem, ListItemAvatar, ListItemText, Paper } from '@mui/material';
+import ParkIcon from '@mui/icons-material/Park';
+import QrCodeIcon from '@mui/icons-material/QrCode';
+import HandymanIcon from '@mui/icons-material/Handyman';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import BarChartIcon from '@mui/icons-material/BarChart';
 
 const HomePage = () => {
  
 
   return (
-    <Grid container bgcolor={'lightblue'}>
-     <Typography variant="h1" color="initial">Home</Typography>
+    <>
+    <Grid container>
+     <Typography variant="h2" color="initial">A cloud based forestry management system that delivers complete transparency and the story behind your wood products.</Typography>
     </Grid>
+
+    <Grid container p={3} justifyContent={'center'}>
+      <Paper>
+      <List>
+        <ListItem>
+          <ListItemAvatar>
+            <ParkIcon />
+            </ListItemAvatar>
+            <ListItemText 
+            primary="Source Transparency" 
+            secondary=" Each tree is logged with details like the lumberjack's name, removal reason, exact GPS location, and a photo." />
+          </ListItem>
+  
+        <ListItem>
+          <ListItemAvatar>
+            <BarChartIcon />
+            </ListItemAvatar>
+            <ListItemText 
+            primary="Processing Insight" 
+            secondary=" From logging to milling, every step is timestamped with operator details and visuals from the sawmill." />
+          </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <VerifiedIcon />
+            </ListItemAvatar>
+            <ListItemText 
+            primary="Quality Assurance" 
+            secondary=" Follow the wood's drying process through comprehensive data and drying history graphs." />
+          </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <HandymanIcon />
+            </ListItemAvatar>
+            <ListItemText 
+            primary="Craftsmanship Tracking" 
+            secondary=" Artisans document their creative process, turning planks into unique products." />
+          </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <QrCodeIcon />
+            </ListItemAvatar>
+            <ListItemText 
+            primary="Final Product Traceability" 
+            secondary="Every product comes with a QR code that links to a detailed report about the wood’s journey and origins." />
+          </ListItem>
+        </List>
+        </Paper>
+      
+    </Grid>
+
+    {/* <Grid container justifyContent={'center'}>
+     <Typography variant="h2" color="initial">Maximize the Value of Your Wood.</Typography>
+    </Grid> */}
+
+    </>
   );
 };
 
